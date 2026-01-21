85718921aa2ebfd3e558ac38e19aab4de1afa684
fengyuanliang3 <fengyuanliang2@jd.com>
2026-01-21 17:53:10 +0800

    fix: 修改 eval_strategy 为 evaluation_strategy 以兼容 transformers 4.38.0

--- lib/python/train.py
+++ lib/python/train.py
@@ -344,7 +344,7 @@ def main():
         learning_rate=args.learning_rate,
         logging_dir=f'{args.output_dir}/logs',
         logging_steps=100,
-        eval_strategy="steps",
+        evaluation_strategy="steps",
         eval_steps=500,
         save_strategy="steps",
         save_steps=500,
